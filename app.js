52c7b8769ce762b431fa3c2c2e3bd7d27f32e4fd
Artem Gladkov <mamifero3004@gmail.com>
2023-05-12 19:46:55 +0200

    Restapi hace su cometido
    Muestro los datos del backend en el frontend con un loader y settimeout simulando la carga

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+const CONTACTS = [
+    { id: 1, name: 'Artem', value: '+34-639-837-382', marked: false },
+    { id: 2, name: 'Pedro', value: '+34-539-337-632', marked: false }
+]
+
+app.get('/api/contacts', (req, res) => {
+    setTimeout(() => {
+        res.status(200).json(CONTACTS)
+    }, 1000);
+})
+
 app.use(express.static(path.resolve(__dirname, 'client')))
 
 app.get('*', (req, res) => {
--- client/frontend.js
+++ client/frontend.js
@@ -1,16 +1,25 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.7.14/dist/vue.esm.browser.js'
 
+Vue.component('loader', {
+    template: `
+        <div style="display: flex;justify-content: center;align-items: center">
+            <div class="spinner-border" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+    `
+})
+
 new Vue({
     el: '#app',
     data() {
         return {
+            loading: false,
             form: {
                 name: '',
                 value: ''
             },
-            contacts: [
-                { id: 1, name: 'Artem', value: '+34-639-837-382', marked: false }
-            ]
+            contacts: []
         }
     },
     computed: {
@@ -31,5 +40,31 @@ new Vue({
         removeContact(id) {
             this.contacts = this.contacts.filter(c => c.id !== id)
         }
+    },
+    async mounted() {
+        this.loading = true
+        this.contacts = await request('/api/contacts')
+        this.loading = false
     }
 })
+
+async function request(url, method = 'GET', data = null) {
+    try {
+        const headers = {}
+        let body
+
+        if (data) {
+            headers['Content-Type'] = 'application/json'
+            body = JSON.stringify(data)
+        }
+
+        const response = await fetch(url, {
+            method,
+            headers,
+            body
+        })
+        return await response.json()
+    } catch (e) {
+        console.warn('Error: e.message')
+    }
+}
