7c97b0728c055d84c964e321fa82f3c875f9f07d
Artem Gladkov <mamifero3004@gmail.com>
2023-05-12 20:14:59 +0200

    El nombre y el valor se añade al servidor

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const path = require('path')
+const { v4 } = require('uuid')
 const app = express()
 
 const CONTACTS = [
-    { id: 1, name: 'Artem', value: '+34-639-837-382', marked: false },
-    { id: 2, name: 'Pedro', value: '+34-539-337-632', marked: false }
+    { id: v4, name: 'Artem', value: '+34-639-837-382', marked: false },
+    { id: v4, name: 'Pedro', value: '+34-539-337-632', marked: false }
 ]
+
+app.use(express.json())
 
 app.get('/api/contacts', (req, res) => {
     setTimeout(() => {
         res.status(200).json(CONTACTS)
     }, 1000);
+})
+
+app.post('/api/contacts', (req, res) => {
+    const contact = { ...req.body, id: v4(), marked: false }
+    CONTACTS.push(contact)
+    res.status(201).json(contact)
 })
 
 app.use(express.static(path.resolve(__dirname, 'client')))
